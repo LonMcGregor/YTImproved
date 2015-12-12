4fa2cf38881f03e55ce8c6b7aaf0e123aa3472aa
Léon McGregor <lonmcgregor@outlook.com>
2015-12-12 15:55:37 +0000

    U: [BUILD] bat build only includes relevant files

--- bwe.bat
+++ bwe.bat
@@ -1,6 +1,5 @@
 @echo off
-del "icons/Thumbs.db"
-7z a -tzip ytimproved.xpi css/*
-7z a -tzip ytimproved.xpi icons/*
-7z a -tzip ytimproved.xpi scripts/*
+7z a -tzip ytimproved.xpi css/*.css
+7z a -tzip ytimproved.xpi icons/*.png
+7z a -tzip ytimproved.xpi scripts/*.js
 7z a -tzip ytimproved.xpi manifest.json
